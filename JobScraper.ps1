a33500012f5d99ed65f43dfc24dd127f40404cd3
Brad77278 <bradleygray888@yahoo.com>
2020-07-27 08:47:48 -0700

    Create JobScraper.ps1

---
+++ JobScraper.ps1
@@ -0,0 +1,10 @@
+﻿$URL = 'https://www.indeed.com/jobs?q=junior%20web%20developer&l=San%20Diego%2C%20CA'
+$SITE = Invoke-WebRequest $URL
+
+($SITE.ParsedHtml.body.getElementsByClassName('title')| Where {
+    $_.ClassName -eq ‘title’
+}).InnerText
+
+($SITE.ParsedHtml.body.getElementsByClassName('summary')| Where {
+    $_.ClassName -eq ‘summary’
+}).InnerText
